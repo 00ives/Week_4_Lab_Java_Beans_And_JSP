5ed934c36a8a024b1d5525e983cb961dca1fe3bf
Ivor Lewis <101133399+00ives@users.noreply.github.com>
2022-09-23 07:03:30 -0600

    Create Bean Added Serializable note bean

--- Week4Lab_SimpleNoteKeeper/src/java/models/Note.java
+++ Week4Lab_SimpleNoteKeeper/src/java/models/Note.java
@@ -1,9 +1,40 @@
 package models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author ivorl
  */
-public class Note {
+public class Note implements Serializable{
+    private String title;
+    private String content;
+
+    public Note() {
+        title= "";
+        content= "";
+    }
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
     
 }
