1c78c027747ae7612865a90ff0550cfd87bef9a9
Ivor Lewis <101133399+00ives@users.noreply.github.com>
2022-09-22 22:42:37 -0600

    Setup note page Built the look of the note page and added the edit link functionality

--- Week4Lab_SimpleNoteKeeper/src/java/servlets/NoteServlet.java
+++ Week4Lab_SimpleNoteKeeper/src/java/servlets/NoteServlet.java
@@ -15,14 +15,31 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String editNoteLink = request.getParameter("edit");
+//        System.out.println("the edit link is " + editNoteLink);
+//        System.out.println( editNoteLink);
+        
+        if (editNoteLink != null){
+        
+        getServletContext()
+                .getRequestDispatcher("/WEB-INF/editnote.jsp")
+                .forward(request, response);
+        
+        }else{
         getServletContext()
                 .getRequestDispatcher("/WEB-INF/viewnote.jsp")
                 .forward(request, response);
+            
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        getServletContext()
+                .getRequestDispatcher("/WEB-INF/editnote.jsp")
+                .forward(request, response);
     }
 
 }
--- Week4Lab_SimpleNoteKeeper/web/WEB-INF/viewnote.jsp
+++ Week4Lab_SimpleNoteKeeper/web/WEB-INF/viewnote.jsp
@@ -5,13 +5,27 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Note Keeper</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Simple Note Keeper</h1>
+        <h2>View Note</h2>
+        <form method="post" action="note">
+            
+        <div><b>Title:</b> This is the title</div>
+        <br>
+        <div><b>Content:</b></div>
+        <div>Contents go here</div>
+        <br>
+            
+        </form>
+        
+        <a href="note?edit" name="edit">Edit</a>
     </body>
 </html>
